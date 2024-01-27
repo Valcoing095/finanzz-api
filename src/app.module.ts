2897ebef16740012aedccc031ba56fe5bbbbca32
Yeison <yvelasco321@gmail.com>
2024-01-26 22:35:18 -0500

    configuarción backend

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { PagosModule } from './pagos/pagos.module';
+
+import { PrestamosModule } from './prestamos/prestamos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ClientsModule, 
+    PrestamosModule, 
+    PagosModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: "1234",
+      database: 'finanzz',
+      autoLoadEntities : true,
+      synchronize: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
